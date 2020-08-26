b4e4817f9c88f2b656faf16fb152f89595aeb7df
Topik <topik@topik.tech>
2020-08-25 22:05:07 -0500

    Wrote the program

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ import java.io.*;
 import java.util.*;
 class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-
+        PrintWriter wrt = new PrintWriter(new FileWriter("test.out"));
+        Scanner scn = new Scanner(new FileReader("test.in"));
+        while (scn.hasNextInt()) wrt.println(scn.nextInt()+1);
+        scn.close();
+         wrt.close(); 
     }
 }
